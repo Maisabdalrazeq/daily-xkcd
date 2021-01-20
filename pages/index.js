e6544be7e146ef281eb19cab40a80db542305149
Maisabdalrazeq <maisabdalrazq@gmail.com>
2021-01-20 20:15:32 +0200

    done

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,28 @@
-import styles from '../styles.module.css'
+import Layout from '../components/Layout'
+import Footer from '../components/Footer'
 
-export default function Home() {
+export default function Home(props) {
   return (
-    <div className={styles.hello}>
-      <p>Hello World</p>
-    </div>
+    <>
+    <Layout>
+        <h1>XKCD</h1>
+        <h2>{props.comic.title}</h2>
+        <img src={props.comic.img} alt={props.comic.alt} />
+        <Footer comicNum={props.comic.num}/>
+    </Layout>
+    </>
   )
+}
+
+export async function getServerSideProps(context){
+    const response = await fetch('http://xkcd.com/info.0.json');
+
+    const data = await response.json();
+
+
+    return{
+        props: { 
+            comic: data 
+        }, 
+    }
 }
